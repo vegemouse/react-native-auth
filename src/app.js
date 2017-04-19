d720c7c99a1c20a47e440cc997796c806529cdb8
Maxwell Cady <maxwell.josef@gmail.com>
2017-04-18 22:01:10 -0700

    create form with no functionality

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header, Button, CardSection, Card } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCs7HthERnDrR6s04O8RnDPxog47hvrEXQ",
+      authDomain: "auth-323e4.firebaseapp.com",
+      databaseURL: "https://auth-323e4.firebaseio.com",
+      projectId: "auth-323e4",
+      storageBucket: "auth-323e4.appspot.com",
+      messagingSenderId: "59947243840"
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
